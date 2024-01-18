dff1aedb3df9db25a914a2a19f4f03f39c83181f
AdriValles <adrivalles4444@gmail.com>
2024-01-18 19:11:29 +0100

    Cambios de if else

--- modaba.java
+++ modaba.java
@@ -10,11 +10,13 @@ public class modaba {
 		String ymodaba = "y modaba";
 		int i =0;
 		char a = 'a';
-		
+		if ( repeticiones <10){
 		for(i=0; i<repeticiones;i++) {
 			System.out.println(ymodaba);
 			ymodaba = ymodaba +a;
-			
+		}}else {
+			System.out.println("Te has pasado")
+		
 			
 		}
 
